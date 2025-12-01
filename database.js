8cf8a413e624084a83b165c9833fb456d7671389
Annu Yadav <128679193+Annu2010@users.noreply.github.com>
2025-12-01 15:43:33 +0530

    Format CREATE TABLE statement for readability

--- database.js
+++ database.js
@@ -5,6 +5,12 @@ export const db = new sqlite3.Database("./orders.db");
 
 db.serialize(() => {
   db.run(
-    "CREATE TABLE IF NOT EXISTS orders (id TEXT PRIMARY KEY, food_item TEXT, quantity INTEGER, address TEXT, phone TEXT, status TEXT)"
+    "CREATE TABLE IF NOT EXISTS orders (
+    id TEXT PRIMARY KEY, 
+    food_item TEXT, 
+    quantity INTEGER, 
+    address TEXT, 
+    phone TEXT, 
+    status TEXT)"
   );
 });
